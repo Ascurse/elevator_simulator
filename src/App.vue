15dd741faebf2babde6c370025507cddb13497f5
Maximilian Shupyro <suomi7862535@gmail.com>
2022-07-13 02:23:28 +0300

    fix: floor array works properly

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="main" v-for="floor in floors.slice().reverse()" :key="floor.id">
     <div class="floor"></div>
-    <button @click="addToElevatorQueue(floor.num)">{{ floor.num }}</button>
+    <div>
+      <button
+        :class="[{ active: floor.favorited }, { not_active: !floor.favorited }]"
+        @click="addToElevatorQueue(floor), (floor.favorited = true)"
+      >
+        {{ floor.num }}
+      </button>
+    </div>
   </div>
   <h1>{{ currentFloor }}</h1>
   <h1>{{ elevatorQueue }}</h1>
@@ -11,29 +18,43 @@
 import { ref, watch } from "vue";
 export default {
   setup() {
+    const floors = [
+      { id: 1, num: 1, favorited: false },
+      { id: 2, num: 2, favorited: false },
+      { id: 3, num: 3, favorited: false },
+      { id: 4, num: 4, favorited: false },
+      { id: 5, num: 5, favorited: false },
+    ];
     let currentFloor = ref(1);
     let elevatorQueue = ref([]);
+    let isMoving = ref(false);
+    let isMovingUp = ref(false);
+    let timer = ref(1);
 
     async function elevatorMove() {
-      if (elevatorQueue.value > 0) {
+      if (elevatorQueue.value.length) {
+        isMoving.value = true;
         console.log("start!");
-        await setTimeout(() => {
-          console.log("setting current floor");
-          setCurrentFloor(elevatorQueue.value[0]);
-          console.log("set!");
-        }, Math.abs(currentFloor.value - elevatorQueue.value[0]) * 1000);
-        setTimeout(() => {
-          console.log("delete");
-          elevatorQueue.value.shift();
-          console.log("del!");
-        }, 3000);
+        timer.value = Math.abs(currentFloor.value - elevatorQueue.value[0].num);
+        isMovingUp.value = elevatorQueue.value[0].num > currentFloor.value;
+        await sleep(timer.value * 1000);
+        console.log("setting current floor");
+        setCurrentFloor(elevatorQueue.value[0].num);
+        console.log("set!");
+        await sleep(3000);
+        console.log("delete");
+        elevatorQueue.value[0].favorited = false;
+        elevatorQueue.value.shift();
+        console.log("del!");
+        isMoving.value = false;
+        elevatorMove();
       }
     }
 
     function addToElevatorQueue(floor) {
       if (
         !elevatorQueue.value.includes(floor) &&
-        currentFloor.value !== floor
+        currentFloor.value !== floor.num
       ) {
         elevatorQueue.value.push(floor);
       }
@@ -43,27 +64,29 @@ export default {
       currentFloor.value = floor;
     }
 
-    watch(elevatorQueue.value, () => {
-      elevatorMove();
-    });
+    function sleep(ms) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+      });
+    }
+
+    watch(
+      () => [...elevatorQueue.value],
+      (newQueue, oldQueue) => {
+        if (newQueue > oldQueue && !isMoving.value) {
+          elevatorMove();
+        }
+      },
+      { deep: false }
+    );
 
     return {
+      floors,
       currentFloor,
       setCurrentFloor,
       elevatorMove,
       elevatorQueue,
       addToElevatorQueue,
-    };
-  },
-  data() {
-    return {
-      floors: [
-        { id: 1, num: 1 },
-        { id: 2, num: 2 },
-        { id: 3, num: 3 },
-        { id: 4, num: 4 },
-        { id: 5, num: 5 },
-      ],
     };
   },
   components: {},
@@ -91,5 +114,14 @@ export default {
   flex-direction: column-reverse;
   height: 150px;
   width: 150px;
+}
+
+.elevator_btn {
+  color: white;
+  cursor: pointer;
+}
+
+.active {
+  background-color: red;
 }
 </style>
